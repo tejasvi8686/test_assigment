bb822bcf78ae1a4c20d5c509b1edecdcc135df4f
tejasvi@appeneure <tejasviraj8686@gmail.com>
2025-02-13 22:17:58 +0530

    Replace default Next.js landing page with basic TenTwenty Farms page structure

---
+++ assets/index.ts
@@ -0,0 +1,11 @@
+import BG1 from "./bg1.webp";
+import BG2 from "./bg2.webp";
+import BG3 from "./bg3.webp";
+import BG4 from "./bg4.webp";
+import Slider1 from "./Slider1.webp";
+import Slider2 from "./Slider2.webp";
+import Slider3 from "./Slider3.webp";
+import Slider4 from "./Slider4.webp";
+
+
+export { BG1, BG2, BG3, BG4, Slider1, Slider2, Slider3, Slider4 };
